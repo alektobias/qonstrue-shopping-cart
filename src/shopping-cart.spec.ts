da51e2c64a25a3dac2e68e5cf7c3e19768b833a6
Alek Tobias <alektobias@gmail.com>
2024-10-27 17:49:10 +0000

    test: adding more of an already added product in the cart

--- src/shopping-cart.spec.ts
+++ src/shopping-cart.spec.ts
@@ -2,21 +2,28 @@ import { beforeEach, describe, expect, test } from "bun:test"
 import { Product, ShoppingCart } from "./shopping-cart"
 
 describe("Shopping Cart", () => {
+  const testProduct: Product = { id: "1", name: "Dove Soap", unitValue: 39.99, quantity: 5 }
+  let shoppingCart: ShoppingCart;
   beforeEach(() => {
-
+    shoppingCart = new ShoppingCart()
   })
   test("Add products to the shopping cart.", () => {
 
-    const shoppingCart = new ShoppingCart()
-    const testProduct: Product = { id: "1", name: "Dove Soap", unitValue: 39.99, quantity: 5 } 
-    
+
     shoppingCart.addProduct(testProduct)
 
-    expect(shoppingCart.cart.products).toEqual({[testProduct.id]: testProduct})
+    expect(shoppingCart.cart.products).toEqual({ [testProduct.id]: testProduct })
     expect(shoppingCart.cart.totalPrice).toEqual(199.95)
 
 
   })
-  test("Add additional products of the same type to the shopping cart.", () => { })
+  test("Add additional products of the same type to the shopping cart.", () => {
+
+    shoppingCart.addProduct(testProduct)
+    shoppingCart.addProduct({ ...testProduct, quantity: 3 })
+
+    expect(shoppingCart.cart.products).toEqual({ [testProduct.id]: { ...testProduct, quantity: 8 } })
+    expect(shoppingCart.cart.totalPrice).toEqual(319.92)
+  })
   test("Calculate the tax rate of the shopping cart with multiple items", () => { })
 })
